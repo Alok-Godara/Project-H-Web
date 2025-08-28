e25e39e194ba6d84efc3d658ccb195e2128d33a1
Alok <b22ci004@iitj.ac.in>
2025-08-28 21:53:03 +0530

    -

--- src/components/ProviderProfile.jsx
+++ src/components/ProviderProfile.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, {useState } from 'react';
 import { User, Mail, Phone, Stethoscope, FileText, Save, ArrowLeft, Eye, EyeOff } from 'lucide-react';
 import { useSelector } from 'react-redux';
-import authSlice from '../store/authSlice.js';
 import { useNavigate } from 'react-router-dom';
 
 
